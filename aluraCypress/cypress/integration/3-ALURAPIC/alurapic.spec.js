ff08c9fc622bcea4231d114d0bdeb900ee835291
ADEVANES THIAGO SILVA DA ROSA <81391055+programadornovato2021@users.noreply.github.com>
2022-03-25 01:07:23 -0300

    y

--- aluraCypress/cypress/integration/3-ALURAPIC/alurapic.spec.js
+++ aluraCypress/cypress/integration/3-ALURAPIC/alurapic.spec.js
@@ -7,7 +7,7 @@ describe('Login e registro de usuarios alura pic', () => {
     });
 
 
-    it.only('Verifica mensagens validacao', () => {
+    it('Verifica mensagens validacao', () => {
        cy.contains('a', 'Register now').click()
        cy.contains('button', 'Register').click()
        cy.contains('ap-vmessage', 'Email is required!').should('be.visible')
@@ -17,4 +17,38 @@ describe('Login e registro de usuarios alura pic', () => {
        cy.contains('ap-vmessage', 'Full name is required!').should('be.visible')
 
     });
+
+
+    it('Verifica mensagens e-mail invalido', () => {
+        cy.contains('a', 'Register now').click()
+        cy.contains('button', 'Register').click()
+        cy.get('input[formcontrolname="email"]').type('Jacqualine')
+        cy.contains('ap-vmessage', 'Invalid e-mail').should('be.visible')
+        
+        
+ 
+     });
+
+
+     it('Verifica mensagens de senha invalida', () => {
+        cy.contains('a', 'Register now').click()
+        cy.contains('button', 'Register').click()
+        cy.get('input[formcontrolname="password"]').type('123')
+        cy.contains('button', 'Register').click()
+        cy.contains('ap-vmessage', 'Mininum length is 8').should('be.visible')
+        
+        
+ 
+     });
+
+     it('Verifica mensagens de o nome do usuario deve estae em minúsculo', () => {
+        cy.contains('a', 'Register now').click()
+        cy.contains('button', 'Register').click()
+        cy.get('input[formcontrolname="userName"]').type('TESTE')
+        cy.contains('button', 'Register').click()
+        cy.contains('ap-vmessage', 'Must be lower case').should('be.visible')
+        
+        
+ 
+     });
 });
