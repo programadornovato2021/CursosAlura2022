f65d2ebed148553c3b426b8079355f3d394b0327
ADEVANES THIAGO SILVA DA ROSA <81391055+programadornovato2021@users.noreply.github.com>
2022-04-01 00:22:58 -0300

    alura04

--- aluraCypress/cypress/integration/3-ALURAPIC/alurapic.spec.js
+++ aluraCypress/cypress/integration/3-ALURAPIC/alurapic.spec.js
@@ -47,8 +47,28 @@ describe('Login e registro de usuarios alura pic', () => {
         cy.get('input[formcontrolname="userName"]').type('TESTE')
         cy.contains('button', 'Register').click()
         cy.contains('ap-vmessage', 'Must be lower case').should('be.visible')
-        
-        
+           
  
      });
+
+     it('verifica mensagens de senha com menos de 8 caracteres', () => {
+      cy.contains('a', 'Register now').click();
+      cy.contains('button', 'Register').click();
+      cy.get('input[formcontrolname="password"]').type('123');
+      cy.contains('button', 'Register').click();
+      cy.contains('ap-vmessage', 'Mininum length is 8').should('be.visible');
+   
+  })
+
+  it('fazer login de usuario valido', () => {
+      cy.login('flavio', '123');
+      cy.contains('a', '(Logout)').should('be.visible');
+  })
+
+  it('fazer login de usuario invalido', () => {
+      cy.login('jacqueline', '1234');
+      cy.on('window:alert', (str) => {
+          expect(str).to.equal('Invalid user name or password');
+      })
+  })
 });
